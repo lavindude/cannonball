db99666d9ba06ec3cdec13cabc544cec307a9d51
lavindude <lavindude@gmail.com>
2022-01-22 21:46:59 -0800

    almost done, polishing a few things

--- Cannon Ball/Assets/Scripts/CollisionEvent.cs
+++ Cannon Ball/Assets/Scripts/CollisionEvent.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CollisionEvent : MonoBehaviour
 {
     private GameManager gameManager;
+    private ParticleSystem explosion;
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager")
             .GetComponent<GameManager>();
+        explosion = GameObject.Find("FX_Explosion_Smoke").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,8 @@ public class CollisionEvent : MonoBehaviour
     {
         if (other.tag == "Cube")
         {
+            explosion.transform.position = transform.position;
+            explosion.Play();
             Destroy(gameObject);
             Destroy(other.gameObject);
             gameManager.updateScore();
